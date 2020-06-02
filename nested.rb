f84ceb5add4045307395acfe7b78542472dc8ee4
Berkley Brown <berkleydeane@gmail.com>
2020-06-02 18:39:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,8 +89,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = [:known_for] = "Ruby", [:languages] = ["LISP", "C"]
-    programmer_hash
+programmer_hash[:yukihiro_matsumoto] 
+    
 end
 
 def changing_alan
